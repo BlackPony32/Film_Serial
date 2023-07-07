a9c5bb625cef52f6b6a9494620b49e8eca24918a
Vlad Kaptenko <vladkaptenko003@gmail.com>
2023-07-07 21:42:32 +0300

    Update main

--- ProgramPack/src/main.py
+++ ProgramPack/src/main.py
@@ -1,21 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from Pages import Page1, Page2, Page3, MainWindow
-from ProgramPack import hello, goodbye
 #from ProgramPack import MyWindowFormat
-from MyWindowFormat import MyMainWindow
+from MainWindow import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MyMainWindow()
-
-    #page1 = Page1()
-    #page2 = Page2()
-    #page3 = Page3()
-
-    #window.add_page(page1)
-    #window.add_page(page2)
-    #window.add_page(page3)
+    window = MainWindow()
 
     window.show()
     sys.exit(app.exec_())
