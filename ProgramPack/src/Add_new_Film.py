d3691667d53f9baa94b760b4614f4847300353c4
Vlad Kaptenko <vladkaptenko003@gmail.com>
2023-07-09 21:52:35 +0300

    Btn now work correct

--- ProgramPack/src/Add_new_Film.py
+++ ProgramPack/src/Add_new_Film.py
@@ -1,45 +1,16 @@
-import sys
-#from MainWindow import MainWindow
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QDesktopWidget, QLabel, QApplication
+from PyQt5.QtWidgets import QVBoxLayout, QWidget, QDesktopWidget, QLabel, QApplication, QHBoxLayout
 from PyQt5.QtGui import QFont, QPixmap
 from MyButton import MyButton
 from MyWindowFormat import MyWindowFormat
+#from ProgramPack.src.MainWindow import _MainWindow
+
 
 class new_Film(MyWindowFormat):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Моя програма PyQt5")
+        self.setWindowTitle("Додати новий фільм")
         self.setGeometry(0, 0, 1000, 700)
-
-        self.setStyleSheet("background-image: url(../img/popcorn4.png);")
-
-        # Створення кнопок і додавання їх до контейнера
-        central_widget = QWidget()
-        layout = QVBoxLayout(central_widget)
-        layout.setSpacing(10)
-        self.setCentralWidget(central_widget)
-
-        # Створення кнопок і додавання їх до контейнера
-        font = QFont()
-        font.setPointSize(16)  # Встановлюємо розмір тексту кнопки
-
-        button1 = MyButton()
-
-        def btn_back_action(self):
-            print("Виконано дію для кнопки 'Назад'")
-            self.close()
-            main = MainWindow()
-            main.show()
-        button1.setText("Button 1")
-        button1.setFont(font)
-        button1.setFixedSize(450, 70)
-        button1.start_animation()
-        button1.clicked.connect(self.openPage2)
-
-        layout.addWidget(button1)
-
 
         self.center()
         self.initialize()
@@ -57,4 +28,50 @@ class new_Film(MyWindowFormat):
         self.move(frame_geometry.topLeft())
 
     def initialize(self):
-        pass
+        # self.setStyleSheet("background-image: url(../img/popcorn4.png);")
+
+        # Створення кнопок і додавання їх до контейнера
+        central_widget = QWidget()
+        #layout = QHBoxLayout(central_widget)
+        #layout.setSpacing(10)
+        self.setCentralWidget(central_widget)
+
+        # Створення кнопок і додавання їх до контейнера
+        font = QFont()
+        font.setPointSize(16)  # Встановлюємо розмір тексту кнопки
+
+        button1 = MyButton()
+        button2 = MyButton()
+        button3 = MyButton()
+
+        button1.setText("Назад")
+        button1.setFont(font)
+        #button1.setGeometry(100, 10, 20, 70)
+        button1.setFixedSize(450, 70)
+        #button1.start_animation()
+        #button1.move(100, 100)
+        button1.clicked.connect(self.open_main_Window)
+
+        button2.setText("Новий переглянутий фільм")
+        button2.setFont(font)
+        button2.setFixedSize(450, 70)
+        #button2.start_animation()
+        #button2.move(300, 100)
+        button2.clicked.connect(self.open_main_Window)
+
+        button3.setText("'Переглянути потім...'")
+        button3.setFont(font)
+        button3.setFixedSize(450, 70)
+        #button3.start_animation()
+        #button3.move(200, 200)
+        button3.clicked.connect(self.open_main_Window)
+
+        central_widget.add
+        #layout.addWidget(button1)
+        #layout.addWidget(button2)
+        #layout.addWidget(button3)
+    def open_main_Window(self):
+        from ProgramPack.src.MainWindow import _MainWindow
+        self.main_Window = _MainWindow()
+        self.main_Window.show()
+        self.close()
