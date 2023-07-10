654a2a34aeae03443b0d98c40cbae0a598d3e999
Vlad Kaptenko <vladkaptenko003@gmail.com>
2023-07-10 22:01:59 +0300

    Fix button location and add base functionality

--- ProgramPack/src/New_Watched_Film.py
+++ ProgramPack/src/New_Watched_Film.py
@@ -2,14 +2,14 @@ from PyQt5.QtWidgets import QVBoxLayout, QWidget, QDesktopWidget, QLabel, QAppli
 from PyQt5.QtGui import QFont, QPixmap
 from MyButton import MyButton
 from MyWindowFormat import MyWindowFormat
-#from ProgramPack.src.MainWindow import _MainWindow
 
 
-class new_Film(MyWindowFormat):
+
+class _new_Watched_Film(MyWindowFormat):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Додати новий фільм")
+        self.setWindowTitle("Новий переглянутий фільм")
         self.setGeometry(0, 0, 1000, 700)
 
         self.center()
@@ -28,50 +28,37 @@ class new_Film(MyWindowFormat):
         self.move(frame_geometry.topLeft())
 
     def initialize(self):
-        # self.setStyleSheet("background-image: url(../img/popcorn4.png);")
-
-        # Створення кнопок і додавання їх до контейнера
-        central_widget = QWidget()
-        #layout = QHBoxLayout(central_widget)
-        #layout.setSpacing(10)
-        self.setCentralWidget(central_widget)
-
-        # Створення кнопок і додавання їх до контейнера
+        self.setStyleSheet("background-image: url(../img/light_cinema.png);")
         font = QFont()
         font.setPointSize(16)  # Встановлюємо розмір тексту кнопки
 
-        button1 = MyButton()
-        button2 = MyButton()
-        button3 = MyButton()
+        button1 = MyButton(self)
+        button2 = MyButton(self)
+        button3 = MyButton(self)
 
         button1.setText("Назад")
         button1.setFont(font)
-        #button1.setGeometry(100, 10, 20, 70)
         button1.setFixedSize(450, 70)
-        #button1.start_animation()
-        #button1.move(100, 100)
-        button1.clicked.connect(self.open_main_Window)
+        button1.start_animation()
+        button1.move(300, 400)
+        button1.clicked.connect(self.open_newFilm)
 
         button2.setText("Новий переглянутий фільм")
         button2.setFont(font)
         button2.setFixedSize(450, 70)
-        #button2.start_animation()
-        #button2.move(300, 100)
-        button2.clicked.connect(self.open_main_Window)
+        button2.start_animation()
+        button2.move(45, 200)
+        button2.clicked.connect(self.open_newFilm)
 
         button3.setText("'Переглянути потім...'")
         button3.setFont(font)
         button3.setFixedSize(450, 70)
-        #button3.start_animation()
-        #button3.move(200, 200)
-        button3.clicked.connect(self.open_main_Window)
+        button3.start_animation()
+        button3.move(510, 200)
+        button3.clicked.connect(self.open_newFilm)
 
-        central_widget.add
-        #layout.addWidget(button1)
-        #layout.addWidget(button2)
-        #layout.addWidget(button3)
-    def open_main_Window(self):
-        from ProgramPack.src.MainWindow import _MainWindow
-        self.main_Window = _MainWindow()
-        self.main_Window.show()
+    def open_newFilm(self):
+        from ProgramPack.src.Add_new_Film import new_Film
+        self.newFilm = new_Film()
+        self.newFilm.show()
         self.close()
