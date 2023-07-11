b938c82069038c38afec47a2bf29125e65c6960b
Vlad Kaptenko <vladkaptenko003@gmail.com>
2023-07-11 21:03:04 +0300

    Update __init__

--- ProgramPack/__init__.py
+++ ProgramPack/__init__.py
@@ -1,2 +1,10 @@
 # Файл __init__.py для пакету ProgramPack
-from .MyWindowFormat import MyMainWindow
+from ProgramPack.src.MainWindow import _MainWindow
+from ProgramPack.src.Add_new_Film import new_Film
+from ProgramPack.src.Add_new_Series import new_Series
+from ProgramPack.src.btn_action import open_new_Film
+from ProgramPack.src.FilmList import _FilmList
+from ProgramPack.src.SeriesList import _SeriesList
+from ProgramPack.src.watch_Film_later import _new_Film_later
+from ProgramPack.src.watch_Series_later import _new_Series_later
+from ProgramPack.src.New_Watched_Series import _new_Watched_Series
