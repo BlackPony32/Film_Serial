eaccc1458a84da7b6cd02aefd5301f7578dca222
Vlad Kaptenko <vladkaptenko003@gmail.com>
2023-07-12 21:29:37 +0300

    Start adding film form description

--- ProgramPack/src/New_Watched_Film.py
+++ ProgramPack/src/New_Watched_Film.py
@@ -1,5 +1,8 @@
-from PyQt5.QtWidgets import QVBoxLayout, QWidget, QDesktopWidget, QLabel, QApplication, QHBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout, QWidget, QDesktopWidget, QLabel, QApplication, QHBoxLayout, QLineEdit, \
+    QFormLayout
 from PyQt5.QtGui import QFont, QPixmap
+from sqlalchemy.dialects.postgresql import psycopg2
+from ProgramPack.src.CustomLabelLine import CustomLabelLineEdit
 from MyButton import MyButton
 from MyWindowFormat import MyWindowFormat
 
@@ -28,7 +31,7 @@ class _new_Watched_Film(MyWindowFormat):
         self.move(frame_geometry.topLeft())
 
     def initialize(self):
-        self.setStyleSheet("background-image: url(../img/light_cinema.png);")
+        #self.setStyleSheet("background-image: url(../img/light_cinema.png);")
         font = QFont()
         font.setPointSize(16)  # Встановлюємо розмір тексту кнопки
 
@@ -48,6 +51,30 @@ class _new_Watched_Film(MyWindowFormat):
         button2.start_animation()
         button2.move(510, 600)
         button2.clicked.connect(self.open_newFilm)
+
+        #
+        widget = QWidget()
+        layout = QFormLayout()
+
+        label1 = CustomLabelLineEdit("Ім'я:", "Введіть ім'я")
+        label1.setFont(QFont("Arial", 12, QFont.Bold))
+        label1.setStyleSheet("color: blue")
+
+        label2 = CustomLabelLineEdit("Прізвище:", "Введіть прізвище")
+        label2.setFont(QFont("Arial", 12, QFont.Bold))
+        label2.setStyleSheet("color: blue")
+
+        label3 = CustomLabelLineEdit("Вік:", "Введіть вік")
+        label3.setFont(QFont("Arial", 12, QFont.Bold))
+        label3.setStyleSheet("color: blue")
+
+        layout.addRow(label1.label, label1.line_edit)
+        layout.addRow(label2.label, label2.line_edit)
+        layout.addRow(label3.label, label3.line_edit)
+        layout.addWidget(widget)
+        self.setLayout(layout)
+
+
 
     def open_newFilm(self):
         from ProgramPack.src.Add_new_Film import new_Film
