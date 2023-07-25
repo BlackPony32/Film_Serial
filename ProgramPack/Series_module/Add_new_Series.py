573abaadf950747bb0291cedddc0e330e8e9ff0f
Vlad Kaptenko <vladkaptenko003@gmail.com>
2023-07-25 17:57:46 +0300

    Bug fix

--- ProgramPack/Series_module/Add_new_Series.py
+++ ProgramPack/Series_module/Add_new_Series.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QVBoxLayout, QWidget, QDesktopWidget, QLabel, QApplication, QHBoxLayout
-from PyQt5.QtGui import QFont, QPixmap
-from MyButton import MyButton
-from MyWindowFormat import MyWindowFormat
-#from ProgramPack.src.MainWindow import _MainWindow
+from PyQt5.QtWidgets import QDesktopWidget
+from PyQt5.QtGui import QFont
+from ProgramPack.src.MyButton import _MyButton
+from ProgramPack.src.MyWindowFormat import MyWindowFormat
+import Image_resource_rc
+#from ProgramPack.my_data.MainWindow import _MainWindow
 
 
 class new_Series(MyWindowFormat):
@@ -16,7 +17,7 @@ class new_Series(MyWindowFormat):
         self.initialize()
 
     def paintEvent(self, event):
-        for button in self.findChildren(MyButton):  # Отримати всі кнопки MyButton
+        for button in self.findChildren(_MyButton):  # Отримати всі кнопки MyButton
             button.paintEvent(event)  # Викликати метод paintEvent для кожної кнопки
 
         super().paintEvent(event)  # Викликати метод paintEvent вікна MainWindow
@@ -34,28 +35,28 @@ class new_Series(MyWindowFormat):
         font = QFont()
         font.setPointSize(16)  # Встановлюємо розмір тексту кнопки
 
-        button1 = MyButton(self)
-        button2 = MyButton(self)
-        button3 = MyButton(self)
+        button1 = _MyButton(self)
+        button2 = _MyButton(self)
+        button3 = _MyButton(self)
 
         button1.setText("Назад")
         button1.setFont(font)
         button1.setFixedSize(400, 70)
-        button1.start_animation()
+        #button1.start_animation()
         button1.move(300, 400)
         button1.clicked.connect(self.open_main_Window)
 
         button2.setText("Новий переглянутий серіал")
         button2.setFont(font)
         button2.setFixedSize(400, 70)
-        button2.start_animation()
+        #button2.start_animation()
         button2.move(45, 200)
         button2.clicked.connect(self.add_New_WatchedSeries)
 
         button3.setText("'Переглянути потім...'")
         button3.setFont(font)
         button3.setFixedSize(450, 70)
-        button3.start_animation()
+        #button3.start_animation()
         button3.move(510, 200)
         button3.clicked.connect(self.add_Series_later)
 
@@ -69,14 +70,14 @@ class new_Series(MyWindowFormat):
 
     # додати новий переглянутий фільм
     def add_New_WatchedSeries(self):
-        from ProgramPack.src.New_Watched_Series import _new_Watched_Series
+        from ProgramPack.Series_module.New_Watched_Series import _new_Watched_Series
         self.watched_series = _new_Watched_Series()
         self.watched_series.show()
         self.close()
 
     # додати фільм переглянути потім
     def add_Series_later(self):
-        from ProgramPack.src.watch_Series_later import _new_Series_later
+        from ProgramPack.Series_module.watch_Series_later import _new_Series_later
         self._new_Series_later = _new_Series_later()
         self._new_Series_later.show()
         self.close()
