75ab97357f1db5597c6536046416114da655d224
Vlad Kaptenko <vladkaptenko003@gmail.com>
2023-07-29 15:52:08 +0300

    Bug fixed

--- ProgramPack/src/Age_rating.py
+++ ProgramPack/src/Age_rating.py
@@ -1,5 +1,7 @@
+import os
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QComboBox, QVBoxLayout, QHBoxLayout, QWidget
+
+from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QComboBox, QVBoxLayout, QHBoxLayout, QWidget, QMessageBox
 
 
 class Age_MovieRatingApp(QMainWindow):
@@ -9,6 +11,7 @@ class Age_MovieRatingApp(QMainWindow):
         self.setGeometry(200, 200, 400, 150)
 
         self.ratings = {
+            "Не обрано": "Не обрано",
             "G": "Для всіх",
             "PG": "Батькам рекомендовано",
             "PG-13": "Для дітей від 13 років",
@@ -25,16 +28,13 @@ class Age_MovieRatingApp(QMainWindow):
 
         self.rating_combo.currentIndexChanged.connect(self.update_description)
 
-        self.description_label = QLabel()
+        self.description_label = QLabel("Не обрано")
         layout.addWidget(self.description_label)
 
         hbox = QHBoxLayout()
         self.submit_button = QPushButton("Зберегти результат")
         self.submit_button.clicked.connect(self.save_to_file)
         hbox.addWidget(self.submit_button)
-
-
-
 
         layout.addLayout(hbox)
 
@@ -44,20 +44,22 @@ class Age_MovieRatingApp(QMainWindow):
 
     def update_description(self):
         selected_rating = self.rating_combo.currentText()
-        description = self.ratings[selected_rating]
-        self.description_label.setText(description)
+        self.description = self.ratings[selected_rating]
+        self.description_label.setText(self.description)
 
     def save_to_file(self):
+        project_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+        file_path = os.path.join(project_dir, "movie_age_rating.txt")
+        self.update_description()
+        # Open the file for writing and save the text
         try:
-            selected_rating = self.rating_combo.currentText()
-            description = self.ratings[selected_rating]
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(self.description)
+            self.close()
+            # QMessageBox.information(self, 'Success', 'Text saved successfully!')
 
-            result = f"Ви обрали віковий рейтинг: {selected_rating}\nОпис: {description}"
-
-            with open("movie_age_rating.txt", "w", encoding="utf-8") as file:
-                file.write(result)
         except Exception as e:
-            self.result_label.setText("Помилка збереження у файл!")
-        self.close()
+            print(f"Error writing to the file: {e}")
+            QMessageBox.critical(self, 'Error', f'Error saving text:\n{str(e)}')
 
 
