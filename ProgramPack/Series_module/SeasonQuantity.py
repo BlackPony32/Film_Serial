bbc17b6f6165ec4c621fe5df949e66e8aae2d688
Vlad Kaptenko <vladkaptenko003@gmail.com>
2023-08-02 22:10:19 +0300

    File Updated

--- ProgramPack/Series_module/SeasonQuantity.py
+++ ProgramPack/Series_module/SeasonQuantity.py
@@ -1,16 +1,31 @@
 import os
 import sys
 
-from PyQt5.QtGui import QFont
+from IPython.external.qt_for_kernel import QtGui
+from PyQt5.QtGui import QFont, QIcon
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QSlider, QVBoxLayout, QPushButton, QMessageBox
 from PyQt5.QtCore import Qt, QPropertyAnimation, QRect
-
+from ProgramPack.src.MyButton import _MyButton
 class SeriesSeasonSelection(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('Вибір кількості сезонів')
         self.setGeometry(400, 300, 450, 180)
+        icon = QIcon(":/images/MovieIcon.jpg")
+        self.setStyleSheet(
+            '''
+            QWidget {
+        background-color: #9dadc7; /* Slightly off-white or light gray */}
+            '''
+        )
+
+        # Встановлюємо картинку як іконку вікна
+
+        width = 32  # Desired width
+        height = 32  # Desired height
+        resized_icon = icon.pixmap(width, height).scaled(width, height)
+        self.setWindowIcon(QtGui.QIcon(resized_icon))
 
         layout = QVBoxLayout()
 
@@ -23,6 +38,30 @@ class SeriesSeasonSelection(QWidget):
         layout.addWidget(self.label_value)
 
         self.slider = QSlider(Qt.Horizontal)
+        self.slider.setStyleSheet(
+    '''
+    QSlider::groove:horizontal {
+        border: 1px solid #999999;
+        height: 10px;
+        background: #CCCCCC;
+        margin: 2px 0;
+    }
+    QSlider::handle:horizontal {
+        background: #FF5733;
+        border: 1px solid #FF5733;
+        width: 18px;
+        height: 18px;
+        margin: -4px 0;
+        border-radius: 9px;
+    }
+    QSlider::add-page:horizontal {
+        background: #0074D9;  /* Колір під мітками */
+    }
+    QSlider::sub-page:horizontal {
+        background: #BDBDBD;  /* Колір під "ручкою" */
+    }
+    '''
+)
         self.slider.setMinimum(1)
         self.slider.setMaximum(16)  # Змінено максимальне значення на 16
         self.slider.setValue(1)
@@ -30,7 +69,7 @@ class SeriesSeasonSelection(QWidget):
         self.slider.setTickPosition(QSlider.TicksBelow)
         layout.addWidget(self.slider)
 
-        self.save_button = QPushButton('Зберегти')
+        self.save_button = _MyButton('Зберегти')
         layout.addWidget(self.save_button)
 
         self.setLayout(layout)
